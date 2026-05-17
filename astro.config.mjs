bd2ada58ccc6ca4cafcb6aa52e6b14583e2bc6aa
vaibhavgoyal09 <vgoyal98145@gmail.com>
2026-05-17 19:52:51 +0530

    Fix LightHouse Performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import critters from "astro-critters";
+import compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [
+    critters(),
+    compress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      Image: false,
+      SVG: false,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
