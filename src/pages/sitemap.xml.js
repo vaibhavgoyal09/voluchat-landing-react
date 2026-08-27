df1e3e94b98d04c151a4ca76c27b9f12001ef0c1
vaibhavgoyal09 <vgoyal98145@gmail.com>
2026-08-27 14:36:28 +0530

    fix sitempa

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -2,7 +2,6 @@ import { fetchPublishedBlogPosts, getBlogApiBaseUrl } from "../lib/blogApi.mjs";
 
 const DEFAULT_SITE_ORIGIN = "https://www.voluchat.com";
 const SITEMAP_BLOG_LIMIT = 100;
-const STATIC_LASTMOD = new Date().toISOString().split("T")[0];
 const STATIC_ROUTES = [
   { path: "/", priority: "1.0" },
   { path: "/features", priority: "0.8" },
@@ -33,7 +32,6 @@ export async function GET({ site }) {
   const entries = [
     ...STATIC_ROUTES.map((route) => ({
       loc: absoluteUrl(route.path, siteOrigin),
-      lastmod: STATIC_LASTMOD,
       priority: route.priority,
     })),
     ...blogs.map((blog) => ({
